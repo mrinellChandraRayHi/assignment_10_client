3d2a935d7f3a93303290ad7013907c25c9c2e855
MrinellChandraRay <mrinellroy@gmail.com>
2024-05-16 02:13:18 +0600

    add navbar

--- src/Components/Components.jsx
+++
@@ -1,9 +0,0 @@
-const Components = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
-
-export default Components;
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Nav from "../Components/Nav";
+
 const Layout = () => {
     return (
         <div>
-            
+            <div><Nav/></div>
+            <div><Outlet/></div>
         </div>
     );
 };
