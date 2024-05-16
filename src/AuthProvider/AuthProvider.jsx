3f2e4e1373144c56334a0c90e2302870495d5c42
MrinellChandraRay <mrinellroy@gmail.com>
2024-05-16 15:08:09 +0600

    add authProvider

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,8 +1,46 @@
-import { createContext } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../Firebase/firebase.config";
 
 export const AuthContext=createContext();
 const AuthProvider = ({children}) => {
-    const authInfo={}
+    // loader
+    const [loader, setLoader]=useState(true);
+    // user
+    const [user, setUser]=useState(null);
+    // createUser
+    const createUser=(email, password)=>{
+        setLoader(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    // signIn
+    const signIn=(email, password)=>{
+        setLoader(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    // signOut
+    const signOUT=()=>{
+        setLoader(true);
+        return signOut(auth);
+    }
+    // obserrving
+    useEffect(()=>{
+        const unsubscribe=onAuthStateChanged(auth, (currentuser)=>{
+            setLoader(false);
+            setUser(currentuser);
+            console.log('observing', currentuser);
+        });
+        return(()=>{
+            unsubscribe();
+        })
+    })
+    const authInfo={
+        loader,
+        createUser,
+        signIn,
+        signOUT,
+        user
+    }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
